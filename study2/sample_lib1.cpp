161e7316be8f80eb168eb58dafcfd377ead26ef4
sakaeda11 <sakaeda11@gmail.com>
2020-11-03 17:55:17 +0900

    add_library study

---
+++ study2/sample_lib1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int samplefunc1() {
+    cout << "LIB1";
+    return 0;
+}
---
+++ study2/sample_lib2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int samplefunc2() {
+    cout << "LIB2";
+    return 0;
+}
